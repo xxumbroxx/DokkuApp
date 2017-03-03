6beb7974afd58b6e7f62b38a1a6fcae4d146ebbb
Grigori <glarab@gmd.com.pe>
2017-03-03 12:19:22 -0500

    Lista

---
+++ app/src/main/java/pe/com/gmd/dokkuapp/view/activity/UsuarioActivity.java
@@ -0,0 +1,15 @@
+package pe.com.gmd.dokkuapp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.com.gmd.dokkuapp.R;
+
+public class UsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+    }
+}
