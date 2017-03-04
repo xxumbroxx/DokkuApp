19a189a7c7e5f9e295f408f79fa2e463fd32339e
Michel A <maguilarp@gm>
2017-03-04 11:08:23 -0500

    Mejoras ¿Cómo llegar? v0.2 FCM

--- app/src/main/java/pe/com/gmd/dokkuapp/remote/FcmIDListenerService.java
+++ app/src/main/java/pe/com/gmd/dokkuapp/remote/FcmIDListenerService.java
@@ -1,25 +1,19 @@
 package pe.com.gmd.dokkuapp.remote;
 
 
-/**
- * Created by jmauriciog on 15/12/2015.
- */
-//public class FcmIDListenerService extends FirebaseInstanceIdService {
-public class FcmIDListenerService {
+import android.util.Log;
 
-//    private FcmTokenService deviceService = new FcmTokenServiceImpl();
-//    private UserService userService = new UserServiceImpl();
-//
-//    @Override
-//    public void onTokenRefresh() {
-//        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-//        Log.d("TOKEN GCM",refreshedToken);
-//        deviceService.setDevice(new FcmToken(refreshedToken,0));
-//
-//        User user = userService.getCurrentUser();
-//        if(user != null){
-//            deviceService.sendRegistrationToServer(refreshedToken,user.getCodigoUsuario());
-//        }
-//
-//    }
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FcmIDListenerService extends FirebaseInstanceIdService {
+
+
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TOKEN FCM",refreshedToken);
+
+    }
 }
